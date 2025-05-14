4c03fc7d69927866544d9872c9accfea45516dd9
KJrTT <kirillqwerttt@gmail.com>
2025-05-14 10:56:50 +0300

    Task1

---
+++ Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Money { get; set; }
+
+
+        public Client(int money , string firstname, int id) 
+        {
+            Id = id;
+            FirstName = firstname;
+            Money = money;
+        }
+        
+
+    }
+}
---
+++ Models/PayPalProcessor.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class PayPalProcessor : IPaymentProcessor
+    {
+
+        private int Id { get; }
+
+        public PayPalProcessor(int id)
+        {
+            Id = id;
+        }
+        public bool ProcessPayment(decimal amount)
+        {
+            return true;
+        }
+        public bool RefundPayment(decimal amount) 
+        {
+            return true;
+        }
+    }
+}
